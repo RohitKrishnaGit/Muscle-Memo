750919063ddd27378cac53a5b78099c568792321
Rohit Krishna <gamevideo2015@gmail.com>
2024-06-25 16:33:22 -0400

    Added controllers and routes

---
+++ Backend/src/controller/CustomExerciseRefController.ts
@@ -0,0 +1,60 @@
+import { AppDataSource } from "../data-source";
+import { NextFunction, Request, Response } from "express";
+import { CustomExerciseRef } from "../entity/CustomExerciseRef";
+import { User } from "../entity/User";
+
+export class CustomExerciseRefController {
+    private customExerciseRefRepository = AppDataSource.getRepository(CustomExerciseRef);
+
+    async all(request: Request, response: Response, next: NextFunction) {
+        const userId = parseInt(request.params.userId);
+
+        return this.customExerciseRefRepository.findBy({
+            user: { id: userId }
+        });
+    }
+
+    async one(request: Request, response: Response, next: NextFunction) {
+        const userId = parseInt(request.params.userId);
+        const id = parseInt(request.params.id);
+
+        const customExerciseRef = await this.customExerciseRefRepository.findOneBy({
+            id,
+            user: { id: userId },
+        });
+
+        if (!customExerciseRef) {
+            return "this customExerciseRef does not exist";
+        }
+        return customExerciseRef;
+    }
+
+    async save(request: Request, response: Response, next: NextFunction) {
+        const { name, userId } = request.body;
+
+        const customExerciseRef = Object.assign(new CustomExerciseRef(), {
+            name,
+            user: { id: userId } as User
+        });
+
+        return this.customExerciseRefRepository.save(customExerciseRef);
+    }
+
+    async remove(request: Request, response: Response, next: NextFunction) {
+        const userId = parseInt(request.params.userId);
+        const id = parseInt(request.params.id);
+
+        let customExerciseRefToRemove = await this.customExerciseRefRepository.findOneBy({
+            id,
+            user: { id: userId },
+        });
+
+        if (!customExerciseRefToRemove) {
+            return "this customExerciseRef does not exist";
+        }
+
+        await this.customExerciseRefRepository.remove(customExerciseRefToRemove);
+
+        return "customExerciseRef has been removed";
+    }
+}

14d7d3b42c42db384bbf64d9a3705a98c930157f
mattl <m4mach@uwaterloo.ca>
2024-06-25 15:48:22 -0400

    Viewmodel skeleton for Friends, Leaderboard, Profile, and HistoryScreen

--- MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/FriendsScreen.kt
+++ MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/FriendsScreen.kt
@@ -7,20 +7,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cs346.musclememo.screens.viewmodels.FriendsScreenViewModel
 
 @Composable
-fun FriendsScreenContent(
-    //viewModel: FriendsScreenViewModel = hiltViewModel()
-){
-    FriendsScreen(
-        //state = state,
-    )
-}
+fun FriendsScreen() {
+    val viewModel = viewModel<FriendsScreenViewModel>()
 
-@Composable
-private fun FriendsScreen(
-    //state:
-) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
---
+++ MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/viewmodels/HistoryScreenViewModel.kt
@@ -0,0 +1,10 @@
+package com.cs346.musclememo.screens.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class HistoryScreenViewModel : ViewModel() {
+    
+    init {
+        // todo: grab user history
+    }
+}

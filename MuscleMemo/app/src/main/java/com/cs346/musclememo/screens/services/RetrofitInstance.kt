f95e4de9167c51938a9f4a4ab1de695669c6abab
Dereck Tu <d8tu@uwaterloo.ca>
2024-07-05 07:05:40 -0400

    use server url instead of localhost

--- MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/services/RetrofitInstance.kt
+++ MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/services/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://10.0.2.2:3000"
+    private const val BASE_URL = "http://107.172.84.88:3000"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()

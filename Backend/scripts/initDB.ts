9807d4d20db43fd0b0dbc4375e28d0bc089cb090
Dereck Tu <d8tu@uwaterloo.ca>
2024-07-16 04:12:04 -0400

    Allow restartdb command to be run while server is running

--- Backend/scripts/initDB.ts
+++ Backend/scripts/initDB.ts
@@ -1,8 +1,26 @@
 import { AppDataSource } from "../src/data-source";
 
+const clearDatabase = async () => {
+    const entities = AppDataSource.entityMetadatas;
+
+    for await (const entity of entities) {
+        const repository = AppDataSource.getRepository(entity.name);
+        await repository.query(`DELETE FROM ${entity.tableName};`);
+    }
+    const migrationTableExists = (
+        await AppDataSource.query(
+            "SELECT count(*) FROM sqlite_master WHERE type='table' AND name='migrations';"
+        )
+    )[0]["count(*)"];
+    if (migrationTableExists) {
+        await AppDataSource.query(`DELETE FROM migrations;`);
+    }
+    await AppDataSource.query(`DELETE FROM sqlite_sequence;`);
+};
+
 AppDataSource.initialize()
     .then(() => {
-        console.log("Data Source has been initialized!");
+        clearDatabase();
     })
     .catch((err) => {
         console.error("Error during Data Source initialization", err);

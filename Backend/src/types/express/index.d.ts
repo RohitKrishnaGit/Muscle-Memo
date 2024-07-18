2897cb12ef451a22d4f5941f9e1aff4d5ef9a8b5
Dereck Tu <d8tu@uwaterloo.ca>
2024-07-18 15:16:31 -0400

    set boolean when the user is referencing self (#114)

--- Backend/src/types/express/index.d.ts
+++ Backend/src/types/express/index.d.ts
@@ -5,6 +5,7 @@ declare global {
     namespace Express {
         interface Request {
             user?: JwtPayload & { id?: number; role?: Role };
+            isMe?: boolean;
         }
     }
 }

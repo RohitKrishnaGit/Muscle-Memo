1f225468809f3c6851b7bd8962ddc7c642629bff
Eric Chen <eric@Erics-MacBook-Air.local>
2024-07-23 17:12:49 -0400

    added variable

--- MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/viewmodels/LeaderboardScreenViewModel.kt
+++ MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/viewmodels/LeaderboardScreenViewModel.kt
@@ -1,6 +1,9 @@
 package com.cs346.musclememo.screens.viewmodels
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.cs346.musclememo.api.RetrofitInstance
 import com.cs346.musclememo.api.services.Records
@@ -12,13 +15,15 @@ import retrofit2.Response
 
 class LeaderboardScreenViewModel : ViewModel() {
     val leaderboardEntries = mutableStateListOf<LeaderboardEntry>()
+    var exerciseRefId by mutableStateOf("1")
 
     init {
         // todo: grab leaderboards
         fetchLeaderboardResults()
     }
     fun fetchLeaderboardResults(){
-        RetrofitInstance.userPrsService.getTopN("1", "50").enqueue(object:
+
+        RetrofitInstance.userPrsService.getTopN(exerciseRefId, "50").enqueue(object:
             Callback<ApiResponse<List<Records>>>{
             override fun onResponse(
                 call: Call<ApiResponse<List<Records>>>,

9e8b1c4495ffbf78bd48ec16228911d7e758cb7f
Eric Chen <eric@Erics-MacBook-Air.local>
2024-07-23 17:09:31 -0400

    leaderboard-wip

--- MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/viewmodels/LeaderboardScreenViewModel.kt
+++ MuscleMemo/app/src/main/java/com/cs346/musclememo/screens/viewmodels/LeaderboardScreenViewModel.kt
@@ -1,10 +1,44 @@
 package com.cs346.musclememo.screens.viewmodels
 
+import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import com.cs346.musclememo.api.RetrofitInstance
+import com.cs346.musclememo.api.services.Records
+import com.cs346.musclememo.api.types.ApiResponse
+import com.cs346.musclememo.classes.LeaderboardEntry
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LeaderboardScreenViewModel : ViewModel() {
+    val leaderboardEntries = mutableStateListOf<LeaderboardEntry>()
 
     init {
         // todo: grab leaderboards
+        fetchLeaderboardResults()
+    }
+    fun fetchLeaderboardResults(){
+        RetrofitInstance.userPrsService.getTopN("1", "50").enqueue(object:
+            Callback<ApiResponse<List<Records>>>{
+            override fun onResponse(
+                call: Call<ApiResponse<List<Records>>>,
+                response: Response<ApiResponse<List<Records>>>
+            ) {
+                if (response.isSuccessful){
+                    response.body()?.data?.let{
+                        for (record in it){
+                            val entry = LeaderboardEntry(username = record.username, value = record.pr)
+                            leaderboardEntries.add(entry)
+                        }
+                        println("$leaderboardEntries")
+                        }
+                    }
+            }
+
+            override fun onFailure(call: Call<ApiResponse<List<Records>>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
